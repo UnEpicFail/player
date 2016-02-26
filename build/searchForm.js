c83343a351b92503433f5f467e3cc84740096b41
UnEpicFail <konstantinkulik86@gmail.com>
2016-02-26 17:23:13 +0300

    full contoll

--- build/searchForm.js
+++ build/searchForm.js
@@ -34,7 +34,7 @@ var Song = React.createClass({
   displayName: "Song",
 
   select: function (e) {
-    this.props.onSelect(e.target.src);
+    this.props.onSelect(this.props.song.url, this.props.index);
   },
   render: function () {
     var song = this.props.song,
@@ -43,7 +43,7 @@ var Song = React.createClass({
     return React.createElement(
       "li",
       null,
-      React.createElement("input", { type: "button", onClick: this.select, src: song.url, value: ">" }),
+      React.createElement("input", { type: "button", onClick: this.select, value: ">" }),
       index,
       " | ",
       song.title
@@ -60,7 +60,8 @@ var Player = React.createClass({
       searchStr: 'Miracle of Sound',
       list: [],
       url: '',
-      state: 'stop'
+      action: 'stop',
+      active: -1
     };
   },
 
@@ -68,8 +69,12 @@ var Player = React.createClass({
     this.setState({ list: list });
   },
 
-  setUrl: function (url) {
-    this.setState({ url: url });
+  setUrl: function (url, index) {
+    console.log(url, index);
+    this.setState({
+      url: url,
+      active: index
+    });
     setTimeout(function () {
       document.getElementsByTagName('audio')[0].play();
     }, 500);
@@ -97,20 +102,86 @@ var Player = React.createClass({
       console.log('error', arguments);
     });
   },
+
   doStop: function () {
     console.log('doStop');
+    this.setState({ action: 'stop' });
     document.getElementsByTagName('audio')[0].pause();
   },
+
   doPause: function () {
     console.log('doPause');
-    document.getElementsByTagName('audio')[0].play();
+    if (this.state.action == 'stop' || this.state.action == 'pause') {
+      if (this.state.url == '') {
+        if (this.state.list.length == 0) {
+          return true;
+        } else {
+          this.setState({
+            url: this.state.list[0].url,
+            active: 0
+          });
+        }
+      }
+      this.setState({ action: 'play' });
+      setTimeout(function () {
+        document.getElementsByTagName('audio')[0].play();
+      }, 500);
+    } else {
+      this.setState({ action: 'pause' });
+      document.getElementsByTagName('audio')[0].pause();
+    }
   },
+
   doNext: function () {
     console.log('doNext');
+    var next;
+    if (this.state.active == -1) {
+      if (this.state.list.length == 0) {
+        return true;
+      } else {
+        next = 0;
+      }
+    } else {
+      next = this.state.active + 1;
+      if (next == this.state.length) {
+        next = 0;
+      }
+    }
+    this.setState({
+      url: this.state.list[next].url,
+      active: next,
+      action: 'play'
+    });
+    setTimeout(function () {
+      document.getElementsByTagName('audio')[0].play();
+    }, 500);
   },
+
   doPrew: function () {
     console.log('doPrew');
+    var prew;
+    if (this.state.active == -1) {
+      if (this.state.list.length == 0) {
+        return true;
+      } else {
+        prew = this.state.list.length - 1;
+      }
+    } else {
+      prew = this.state.active - 1;
+      if (prew == -1) {
+        prew = this.state.list.length - 1;
+      }
+    }
+    this.setState({
+      url: this.state.list[prew].url,
+      active: prew,
+      action: 'play'
+    });
+    setTimeout(function () {
+      document.getElementsByTagName('audio')[0].play();
+    }, 500);
   },
+
   addListeners: function () {
     var element = document.getElementsByTagName('body')[0];
     element.addEventListener('STOP', this.doStop);
@@ -118,6 +189,7 @@ var Player = React.createClass({
     element.addEventListener('NEXT', this.doNext);
     element.addEventListener('PREW', this.doPrew);
   },
+
   render: function () {
     this.addListeners();
     var list = this.state.list;
--- src/searchForm.js
+++ src/searchForm.js
@@ -30,7 +30,7 @@ var SearchForm = React.createClass({
 
 var Song = React.createClass({
   select: function(e){
-    this.props.onSelect(e.target.src);
+    this.props.onSelect(this.props.song.url, this.props.index);
   },
   render: function(){
     var song = this.props.song,
@@ -38,7 +38,7 @@ var Song = React.createClass({
 
     return (
       <li>
-        <input type="button" onClick = {this.select} src = {song.url} value = '>' />
+        <input type="button" onClick = {this.select} value = '>' />
         {index} | {song.title}
       </li>
     )
@@ -52,7 +52,8 @@ var Player = React.createClass({
       searchStr:'Miracle of Sound',
       list: [],
       url: '',
-      state: 'stop',
+      action: 'stop',
+      active: -1
     };
   },
 
@@ -61,8 +62,12 @@ var Player = React.createClass({
     this.setState({list:list});
   },
 
-  setUrl: function (url) {
-    this.setState({url: url});
+  setUrl: function (url, index) {
+    console.log(url, index);
+    this.setState({
+      url: url,
+      active: index
+    });
     setTimeout(function(){
         document.getElementsByTagName('audio')[0].play();
     },500);
@@ -94,16 +99,87 @@ var Player = React.createClass({
       console.log('error', arguments);
     })
   },
+
   doStop:function(){
-    console.log('doStop')
+    console.log('doStop');
+    this.setState({action:'stop'});
     document.getElementsByTagName('audio')[0].pause();
   },
+
   doPause:function(){
-    console.log('doPause')
-    document.getElementsByTagName('audio')[0].play();
+    console.log('doPause');
+    if (this.state.action == 'stop' || this.state.action == 'pause'){
+      if(this.state.url == ''){
+        if(this.state.list.length == 0){
+          return true;
+        }else{
+          this.setState({
+            url:this.state.list[0].url,
+            active: 0
+          });
+        }
+      }
+      this.setState({action:'play'});
+      setTimeout(function(){
+        document.getElementsByTagName('audio')[0].play();
+      }, 500)
+
+    } else {
+      this.setState({action:'pause'});
+      document.getElementsByTagName('audio')[0].pause();
+    }
   },
-  doNext:function(){console.log('doNext')},
-  doPrew:function(){console.log('doPrew')},
+
+  doNext:function(){
+    console.log('doNext');
+    var next;
+    if(this.state.active == -1){
+      if(this.state.list.length == 0){
+        return true;
+      } else {
+        next = 0
+      }
+    } else {
+      next = this.state.active + 1;
+      if(next == this.state.length){
+        next = 0;
+      }
+    }
+    this.setState({
+      url: this.state.list[next].url,
+      active: next,
+      action: 'play'
+    })
+    setTimeout(function(){
+      document.getElementsByTagName('audio')[0].play();
+    }, 500)
+  },
+
+  doPrew:function(){
+    console.log('doPrew')
+    var prew;
+    if(this.state.active == -1){
+      if(this.state.list.length == 0){
+        return true;
+      } else {
+        prew = this.state.list.length - 1
+      }
+    } else {
+      prew = this.state.active - 1;
+      if(prew == -1){
+        prew = this.state.list.length - 1;
+      }
+    }
+    this.setState({
+      url:this.state.list[prew].url,
+      active: prew,
+      action: 'play'
+    })
+    setTimeout(function(){
+      document.getElementsByTagName('audio')[0].play();
+    }, 500)
+  },
+
   addListeners: function () {
     var element = document.getElementsByTagName('body')[0];
     element.addEventListener('STOP', this.doStop);
@@ -111,6 +187,7 @@ var Player = React.createClass({
     element.addEventListener('NEXT', this.doNext);
     element.addEventListener('PREW', this.doPrew);
   },
+
   render: function() {
     this.addListeners()
     var list = this.state.list
